83b116fdea2cdb41480aa08236de5d2ca6ed4ad6
Isaac <ainomugish@gmail.com>
2015-09-21 11:07:31 +0300

    fixed migrations

--- migrations/m150919_035001_add_fk_to_status_table.php
+++ migrations/m150919_035001_add_fk_to_status_table.php
@@ -7,13 +7,13 @@ class m150919_035001_add_fk_to_status_table extends Migration
 {
     public function up()
     {
-        //$this->addColumn('{{%status}}', 'user_id', Schema::TYPE_INTEGER . ' NOT NULL');
+        $this->addColumn('{{%status}}', 'user_id', Schema::TYPE_INTEGER . ' NOT NULL');
         $this->addForeignKey('fk_user_status','{{%status}}','id', '{{%user}}', 'id', 'CASCADE', 'CASCADE');
     }
 
     public function down()
     {
-        //$this->dropColumn('{{%status}}', 'user_id');
+        $this->dropColumn('{{%status}}', 'user_id');
         $this->dropForeignKey('fk_user_status','status');
 
         return false;
